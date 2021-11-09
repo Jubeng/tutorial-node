f212cdb4ca55cd53caca908d12ac2cbdc907a52a
Jubeng <jovenpaja@gmail.com>
2021-11-09 19:56:57 +0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const oName = require('./4-name');
+const oSayHi = require('./5-utils');
+const data = require('./6-alternative-module');
+require('./7-mind-grenade');
+oSayHi('susan');
+oSayHi(oName.john);
+oSayHi(oName.peter);
---
+++ 5-utils.js
@@ -0,0 +1,5 @@
+const sayHi = (sName) => {
+    console.log(`Hello there ${sName}`);
+}
+
+module.exports = sayHi;
